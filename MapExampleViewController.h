1e9eb0fa13b784518bf69838fc92066ed2b46ccb
Алексей <alexbushmelev@icloud.com>
2016-09-20 19:15:27 +0300

    add Map Controller

---
+++ MapExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapExampleViewController.h
+//  OtherControllers
+//
+//  Created by Alex_Bushmelev on 03.09.15.
+//  Copyright (c) 2015 Alex_Bushmelev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapExampleViewController : UIViewController
+
+@end
